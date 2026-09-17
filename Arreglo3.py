f1387ba5d57dcb5a1c2c62a3b476dbcc31aa9198
Yisusrm <jesuselimorales05@gmail.com>
2026-09-17 14:14:38 -0600

    Add files via upload

---
+++ Arreglo3.py
@@ -0,0 +1,10 @@
+#Ordenamiento de lista sin swapped
+lista = [4, 2, 6, 8, 5, 7]
+print(lista)
+for i in range(len(lista)):
+    for x in range(len(lista)-1):
+        if lista[x]>lista[x+1]:
+            aux=lista[x]
+            lista[x]=lista[x+1]
+            lista[x+1]=aux
+print("Lista ordenada:", lista)
